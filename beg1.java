535675e8ac40c557194af649b42fd276739909ee
shibinshaji <47378205+shibinshaji@users.noreply.github.com>
2019-02-06 12:08:47 +0530

    Create beg1.java

---
+++ beg1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class beg1
+{
+    public static void main(String[] args)
+    {
+        int n=3;
+        
+        if (n < 0)
+        {
+            System.out.println("Negative");
+        }
+        else if(n > 0)
+        {
+         System.out.println("Positive");   
+        }
+        else
+        {
+            System.out.println("Zero");
+        }
+    }
+}
